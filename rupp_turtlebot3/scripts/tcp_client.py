994886210b36e06164827f07c49154121b74f412
Matthias Rupp <mru3383@students.fhv.at>
2021-03-18 09:37:12 -0700

    client now in robot callback, upgraded launch file

--- rupp_turtlebot3/scripts/tcp_client.py
+++ rupp_turtlebot3/scripts/tcp_client.py
@@ -2,28 +2,55 @@
 
 import socket
 import rospy
+import sys, getopt
+from sensor_msgs.msg import LaserScan
 
 class Client:
-    def __init__(self):
+    def __init__(self, name):
         self.TCP_IP = '127.0.0.1'
         self.TCP_PORT = 5005
         self.BUFFER_SIZE = 1024
         self.MESSAGE = "Hello, World!"
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        full_sub = name + "/scan"
+        print(full_sub)
+        self.sub = rospy.Subscriber(full_sub, LaserScan, self.scan_callback)
 
     def send(self, message):
-        self.sock.connect((self.TCP_IP, self.TCP_PORT))
-        self.sock.send(message.encode())
-        data = self.sock.recv(self.BUFFER_SIZE)
-        self.sock.close()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((self.TCP_IP, self.TCP_PORT))
+        sock.send(message.encode())
+        data = sock.recv(self.BUFFER_SIZE)
+        sock.close()
         print("received data: ", data)
-
-if __name__=='__main__':
+    
+    def scan_callback(self, scan:LaserScan):
+        self.send("Laser data!!!")
+    
+def main(argv):
+    name = ''
+    try:
+        opts, args = getopt.getopt(argv,"hn:",["name="])
+    except getopt.GetoptError:
+        print('tcp_client.py -n <name>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('test.py -n <name>')
+            sys.exit()
+        elif opt in ("-n", "--name"):
+            name = arg
+    print('Groupname is "', name)
     rospy.init_node('rupp_tcp_client', anonymous=True)
-    client = Client()
-    client.send("SENDING DATA")
+    client = Client(name)
+    # client.send("SENDING DATA")
     try:
         rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down")
+    
+
+if __name__=='__main__':
+    main(sys.argv[1:])
+    
+    
 
