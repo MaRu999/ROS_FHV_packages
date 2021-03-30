6548cae24517503455dfa270d542dded33047cd4
Matthias Rupp <mru3383@students.fhv.at>
2021-03-30 14:26:50 -0700

    Added script/worlds/launch files for going to nearest object

---
+++ rupp_turtlebot3/scripts/go_to_nearest.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import rospy 
+import math 
+from operator import itemgetter
+from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import Twist
+
+LINEAR_VEL = 0.22
+STOP_DISTANCE = 0.2
+LIDAR_ERROR = 0.05
+SAFE_STOP_DISTANCE = STOP_DISTANCE + LIDAR_ERROR
+MIN_DISTANCE = 3.5
+
+class GoTo:
+    def __init__(self):
+        self._cmd_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        self.scan()
+
+    def scan(self):
+        while not rospy.is_shutdown():
+            scan = rospy.wait_for_message('scan', LaserScan)
+            min_distance = min(scan.ranges)
+            min_index = min(enumerate(scan.ranges), key=itemgetter(1))[0]
+            linear = LINEAR_VEL
+            if min_distance < MIN_DISTANCE and min_distance > SAFE_STOP_DISTANCE:
+                if min_index > 180:
+                    angular = math.radians(min_index-360)
+                else:
+                    angular = math.radians(min_index)
+            elif min_distance < SAFE_STOP_DISTANCE:
+                linear = 0.0
+                angular = 0.0
+            else:
+                angular = 0.0
+            twist = Twist()
+            twist.angular.z = angular 
+            twist.linear.x = linear
+            self._cmd_pub.publish(twist)
+            print(angular)
+
+def main():
+    rospy.init_node('goto_nearest')
+    try:
+        gt = GoTo()
+    except KeyboardInterrupt:
+        print("Shutting down")
+
+
+if __name__ == '__main__':
+    main()
