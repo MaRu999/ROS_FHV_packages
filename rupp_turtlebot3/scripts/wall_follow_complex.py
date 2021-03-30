8651123160bb181f8406a35b1011768d3294c4cd
Matthias Rupp <mru3383@students.fhv.at>
2021-03-30 15:31:17 -0700

    Improved wall follow (now partly proportional)

--- rupp_turtlebot3/scripts/wall_follow_complex.py
+++ rupp_turtlebot3/scripts/wall_follow_complex.py
@@ -14,72 +14,41 @@ SAFE_STOP_DISTANCE = STOP_DISTANCE + LIDAR_ERROR
 class Obstacle():
     def __init__(self):
         self._cmd_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
-        self.obstacle()
+        self.wall_follow()
 
-    def get_scan_try(self):
+    def get_scan(self):
         scan = rospy.wait_for_message('scan', LaserScan)
-        scan_filter = []
-        rf = scan.ranges[9]
-        rdf = scan.ranges[44]
-        r = scan.ranges[89]
-        rdb = scan.ranges[134]
-        rb = scan.ranges[169]
-        lb = scan.ranges[189]
-        ldb = scan.ranges[224]
-        l = scan.ranges[269]
-        ldf = scan.ranges[314]
-        lf = scan.ranges[349]
-        scan_filter.append(rf)
-        scan_filter.append(rdf)
-        scan_filter.append(r)
-        scan_filter.append(rdb)
-        scan_filter.append(rb)
-        scan_filter.append(lb)
-        scan_filter.append(ldb)
-        scan_filter.append(l)
-        scan_filter.append(ldf)
-        scan_filter.append(lf)
-        return scan_filter
+        return scan.ranges
 
-    def obstacle(self):
+    def wall_follow(self):
         twist = Twist()
         turtlebot_moving = True
 
         while not rospy.is_shutdown():
-            # order: rf, rdf, r, rdb, rb, lb, ldb, l, ldf, lf
-            lidar_distances = self.get_scan_try()
+            lidar_distances = self.get_scan()
             min_distance = min(lidar_distances)
             min_index = min(enumerate(lidar_distances), key=itemgetter(1))[0]
-            lf = lidar_distances[0]
-            ldf = lidar_distances[1]
-            l = lidar_distances[2]
-            ldb = lidar_distances[3]
-            lb = lidar_distances[4]
-            rb = lidar_distances[5]
-            rdb = lidar_distances[6]
-            r = lidar_distances[7]
-            rdf = lidar_distances[8]
-            rf = lidar_distances[9]
-            left_turn = math.pi/2
-            small_left_turn = math.pi/4
-            if lf < SAFE_STOP_DISTANCE:
-                print("LF")
-                twist.linear.x = 0.0
-                twist.angular.z = left_turn
-                self._cmd_pub.publish(twist)
-            elif r < SAFE_STOP_DISTANCE:
-                print("R")
-                twist.linear.x = LINEAR_VEL
-                twist.angular.z = -math.pi/16
-                self._cmd_pub.publish(twist)
-            else:
-                print("Going straight...")
-                twist.linear.x = LINEAR_VEL
-                twist.angular.z = 0.0
-                self._cmd_pub.publish(twist)
+            angular = 0.0
+            linear = LINEAR_VEL
+            if min_distance < SAFE_STOP_DISTANCE * 2:
+                if min(lidar_distances[:10]) < SAFE_STOP_DISTANCE or min(lidar_distances[354:364]) < SAFE_STOP_DISTANCE:
+                    angular = math.radians(-90)
+                    linear = 0.0
+                else:
+                    if min_index > 180:
+                        angular = math.radians(min_index-360-90)
+                    else:
+                        angular = math.radians(min_index-90)
+            twist = Twist()
+            twist.angular.z = angular
+            twist.linear.x = linear
+            self._cmd_pub.publish(twist)
+            print(angular)
+
+            
 
 def main():
-    rospy.init_node('turtlebot3_obstacle_test')
+    rospy.init_node('rupp_wall_follow_complex')
     try:
         obstacle = Obstacle()
     except rospy.ROSInterruptException:
